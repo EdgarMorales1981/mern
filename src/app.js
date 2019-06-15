8a4dcd8716f01f7733b3f776afbac3827fa09191
EdgarMorales1981 <desarrolloaplicaciones1981@gmail.com>
2019-06-14 21:23:38 -0400

    primera subida al servidor

---
+++ src/app.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+
+//connecting in db
+
+mongoose.connect('mongodb://localhost/crud-mongo')
+
+.then(db => console.log('db conectada'))
+.catch(err => console.log(err));
+
+
+//importing routes
+const indexRoutes = require('./routes/index.js');
+
+
+//settings
+app.set('port',process.env.PORT || 4444);
+
+app.set('views', path.join(__dirname, 'views'));
+
+app.set('view engine','ejs');
+
+//middlewares express
+app.use(morgan('dev'));
+app.use(express.urlencoded({extended:false}));
+
+
+
+//routes
+
+app.use('/', indexRoutes);
+
+// starting the server
+app.listen(app.get('port'),() => {
+
+console.log(`server on port ${app.get('port')}`);
+
+});
+
